636e23413c86464efac48f64aa8d6a579ee0fce9
Ray-Yuan Liu <louis383@gmail.com>
2017-10-16 20:00:39 +0800

    add start game countdown timer and shot machine screen to start.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,40 +12,66 @@ public class GameController : MonoBehaviour {
     public float spawnDecrement = 0.1f;
     public float minimumSpawnDuration = 0.5f;
     public float gameTimer = 15f;
+    private bool gameStart = false;
+    public bool IsGameStart {
+        get { return gameStart; }
+    }
 
     private Mole[] moles;
     private float spawnTimer = 0f;
-    private float resetTimer = 3f;
+    private float defaultGameTime = 0f;
+    private float hintTimer = 3f;
+    private float gameplayCountdownTimer = 3f;
 
     // Use this for initialization
     void Start() {
         moles = moleContainer.GetComponentsInChildren<Mole>();
+        defaultGameTime = gameTimer;
+        infoText.text = "shot here to play";
     }
 
     // Update is called once per frame
     void Update() {
-        gameTimer -= Time.deltaTime;
-        if (gameTimer > 0f) {
-            infoText.text = "Hit all the moles!\nTime: " + Mathf.Floor(gameTimer) + "\nScore: " +
-                            player.score;
+        if (!gameStart) {
+            return;
+        }
 
-            spawnTimer -= Time.deltaTime;
-            if (spawnTimer <= 0f) {
-                moles[Random.Range(0, moles.Length)].Rise();
+        gameplayCountdownTimer -= Time.deltaTime;
+        if (gameplayCountdownTimer <= 0f) {
+            gameTimer -= Time.deltaTime;
+            if (gameTimer > 0f) {
+                infoText.text = "Hit all the moles!\nTime: " + Mathf.Floor(gameTimer) +
+                                "\nScore: " +
+                                player.score;
 
-                spawnDuration -= spawnDecrement;
-                if (spawnDuration < minimumSpawnDuration) {
-                    spawnDuration = minimumSpawnDuration;
+                spawnTimer -= Time.deltaTime;
+                if (spawnTimer <= 0f) {
+                    moles[Random.Range(0, moles.Length)].Rise();
+
+                    spawnDuration -= spawnDecrement;
+                    if (spawnDuration < minimumSpawnDuration) {
+                        spawnDuration = minimumSpawnDuration;
+                    }
+                    spawnTimer = spawnDuration;
                 }
-                spawnTimer = spawnDuration;
+            } else {
+                infoText.text = "Game over\nYour score: " + player.score;
+                hintTimer -= Time.deltaTime;
+                if (hintTimer <= 0f) {
+                    gameStart = false;
+                    infoText.text += "\n\nshot here\nto restart game";
+                }
             }
         } else {
-            infoText.text = "Game over\nYour score: " + player.score;
-
-            resetTimer -= Time.deltaTime;
-            if (resetTimer <= 0f) {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
+            infoText.text = "Ready?  " + (Mathf.Floor(gameplayCountdownTimer) + 1);
         }
+    }
+
+    public void StartGame() {
+        gameTimer = defaultGameTime;
+        player.score = 0;
+        gameplayCountdownTimer = 3f;
+        gameStart = true;
+        hintTimer = 3f;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,21 +8,31 @@ public class Player : MonoBehaviour {
     public Hammer hammer;
     public int score = 0;
 
+    private GameController gameController;
+
     // Use this for initialization
-    void Start() { }
+    void Start() {
+        gameController = FindObjectOfType<GameController>();
+    }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Space)) {
             RaycastHit raycastHit;
             if (Physics.Raycast(transform.position, mainCamera.transform.forward, out raycastHit)) {
-                if (raycastHit.transform.GetComponent<Mole>() != null) {
+                Debug.Log("ray hit something: " + raycastHit.transform.name);
+                bool gameStart = gameController.IsGameStart;
+                if (raycastHit.transform.GetComponent<Mole>() != null && gameStart) {
                     Mole mole = raycastHit.transform.GetComponent<Mole>();
                     bool isMoleAttackable = mole.OnHit();
                     hammer.Hit(mole.transform.position);
                     if (isMoleAttackable) {
                         score++;
                     }
+                }
+
+                if (raycastHit.transform.name.Equals("machine") && !gameStart) {
+                    gameController.StartGame();
                 }
             }
         }
